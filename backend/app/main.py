d51895af29f277240dcc4a84891a3bd2d8c61e4f
shibata_konosuke <shibata_k@r.recruit.co.jp>
2025-03-02 23:10:47 +0900

    ローカル実行テスト、curlを使ったDockerコンテナ間の通信を確認

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+# .envを読み込む
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI"}
+    return {"message": f"Hello from FastAPI! Debug Mode: {os.getenv('DEBUG')}"}
