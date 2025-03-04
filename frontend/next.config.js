34926775df454b19c77a959841aa4b625b982849
shibata_konosuke <shibata_k@r.recruit.co.jp>
2025-03-04 09:32:48 +0900

    frontからgithub APIに通信できないエラーの解消

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // App Routerを有効化
-  },
   env: {
     BACKEND_URL: process.env.BACKEND_URL || "http://backend:8000",
   },
